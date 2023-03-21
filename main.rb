e6ddd63091a9983431b8a16f608bf7a14395243d
Faramarz <faramarz.hosseini@divar.ir>
2023-03-21 21:12:47 +0330

    Read db path from env var

--- main.rb
+++ main.rb
@@ -2,5 +2,5 @@
 
 require_relative 'mention_counter'
 
-mention_counter = MentionCounter.new('mentions.db', ENV['BOT_TOKEN'])
+mention_counter = MentionCounter.new(ENV['SQLITE_DB_PATH'], ENV['BOT_TOKEN'])
 mention_counter.run
