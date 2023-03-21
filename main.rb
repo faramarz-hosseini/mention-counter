b960f5283b352a75b5be621411a81c0259790274
Faramarz <faramarz.hosseini@divar.ir>
2023-03-21 21:11:23 +0330

    Use env var to read bot token

--- main.rb
+++ main.rb
@@ -2,6 +2,5 @@
 
 require_relative 'mention_counter'
 
-mention_counter = MentionCounter.new('test.db',
-                                     'MTA4NDQwMDI2MzUzNzg4NTI4NA.GpKyMJ.VQvkUB1LzrRgVxDc1T0spbN_t2oFfHUnxbub7Y')
+mention_counter = MentionCounter.new('mentions.db', ENV['BOT_TOKEN'])
 mention_counter.run
